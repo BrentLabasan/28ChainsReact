009e93ee48589af08124a9a7a47e3ba69e8c3611
BrentLabasan <brentlabasan@gmail.com>
2016-03-27 23:36:44 -0700

    end of Building Applications with React and Flux 4.2

---
+++ src/components/homePage.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var React = require('react');
+
+var attemptStreak = React.createClass({
+    render: function() {
+        return (
+            <div>
+                attemptStreak
+            </div>
+        );
+    }
+});
+
+module.exports = attemptStreak;
