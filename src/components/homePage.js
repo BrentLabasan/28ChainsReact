e20e1a5fb4a8e8da811b48f8be4008b71a8950b3
BrentLabasan <brentlabasan@gmail.com>
2016-03-28 22:03:55 -0700

    finished https://www.firebase.com/docs/web/libraries/react/quickstart.html but it doesn't exactly have you being sure that it's connected, unlike the AngularFire guide

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -3,6 +3,11 @@
 var React = require('react');
 
 var attemptStreak = React.createClass({
+    mixins: [ReactFireMixin],
+    componentWillMount: function() {
+        var ref = new Firebase("https://28chains.firebaseio.com/");
+        this.bindAsArray(ref, "items");
+    },
     render: function() {
         return (
             <div>
