8c0347029e1eb52250e4346b6deb9b93664e218f
BrentLabasan <brentlabasan@gmail.com>
2016-03-28 23:31:51 -0700

    I think the mixins might be working

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,13 +1,54 @@
 "use strict";
 
 var React = require('react');
+var Firebase = require('firebase');
+var Reactfire = require('reactfire');
 
 var attemptStreak = React.createClass({
-    mixins: [ReactFireMixin],
+
+    mixins: [Reactfire.ReactFireMixin],
+
+
+    // without ReactFire
+/*    mixins: [ReactFireMixin],
+
     componentWillMount: function() {
         var ref = new Firebase("https://28chains.firebaseio.com/");
         this.bindAsArray(ref, "items");
     },
+
+    handleSubmit: function(e) {
+        e.preventDefault();
+        this.firebaseRefs.items.push({
+            text: this.state.text
+        });
+        this.setState({ text: "" });
+    },*/
+
+    // with ReactFire
+
+    /*    componentWillMount: function() {
+            this.firebaseRef = new Firebase("https://28chains.firebaseio.com/");
+            this.firebaseRef.on("child_added", function(dataSnapshot) {
+                this.items.push(dataSnapshot.val());
+                this.setState({
+                    items: this.items
+                });
+            }.bind(this));
+        },
+
+        handleSubmit: function(e) {
+            e.preventDefault();
+            this.firebaseRef.push({
+                text: this.state.text
+            });
+            this.setState({text: ""});
+        },*/
+
+/*    componentWillUnmount: function() {
+        this.firebaseRef.off();
+    },*/
+
     render: function() {
         return (
             <div>
